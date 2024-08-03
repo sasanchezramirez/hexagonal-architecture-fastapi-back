e22b69810a2ce4b38debfa655d7d3d9e8eba87fa
santiago <sasanchezramirez@gmail.com>
2024-08-02 19:52:08 -0500

    feat: db connection ok

--- app/infrastructure/driven_adapter/persistence/entity/user_entity.py
+++ app/infrastructure/driven_adapter/persistence/entity/user_entity.py
@@ -3,7 +3,7 @@ from app.infrastructure.driven_adapter.persistence.config.database import Base
 
 
 class User_entity(Base):
-    __tablename__ = "kiireoffice_user"
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
